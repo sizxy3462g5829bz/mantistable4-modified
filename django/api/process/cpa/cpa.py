105f7eef5694a56407f8d622582ac546766f801f
Cilium <andreadevsrv@gmail.com>
2020-05-26 20:19:13 +0200

    Added missing cpa/revision files

---
+++ django/api/process/cpa/cpa.py
@@ -0,0 +1,54 @@
+import collections
+
+class CPAProcess:
+    def __init__(self, cea):
+        self._cea = cea
+
+    def compute(self):
+        table_triples = []
+
+        for _, (_, linkages) in enumerate(self._cea):        
+            triples = [
+                [
+                    (link.get_triple(), link.get_confidence())
+                    for link in linkage
+                ]
+                for linkage in linkages
+            ]
+            table_triples.append(triples)
+        
+        predicates = []        
+        if len(table_triples) > 0:
+            columns_count = len(table_triples[0])
+            
+            for _ in range(0, columns_count):
+                predicates.append({})
+            
+            for triples in table_triples:
+                for col_idx, links in enumerate(triples):
+                    link_preds = {}
+                    for link in links:
+                        triple, confidence = link
+                        
+                        predicate = triple[1]
+                        
+                        if predicate not in link_preds:
+                            link_preds[predicate] = 0.0
+                        
+                        # TODO: remove this
+                        link_preds[predicate] = max(link_preds[predicate], confidence)
+
+                    for predicate in link_preds:
+                        if predicate not in predicates[col_idx]:
+                            predicates[col_idx][predicate] = 0.0
+                        
+                        predicates[col_idx][predicate] += 1
+        
+        if len(self._cea) != 0:
+            for col_idx, d in enumerate(predicates):
+                norm_factor = len(self._cea)
+                if norm_factor != 0.0:
+                    for pred in d:
+                        predicates[col_idx][pred] /= norm_factor
+        
+        return predicates
---
+++ django/api/process/revision/revision.py
@@ -0,0 +1,71 @@
+class RevisionProcess:
+    def __init__(self, cea, cpa):
+        self._cea = cea
+        self._cpa = cpa
+
+    def compute(self):
+        # CEA revision
+        row_results = []
+        for _, (subjects, linkages) in enumerate(self._cea):              
+            winning_row_subject = None
+            winning_row_linkages = []
+            winning_row_confidence = 0.0
+                
+            # Take subjects with maximum score, leave the rest. This is for performance reasons
+            subject_items = sorted(
+                subjects.items(),   # (subject, confidence)
+                key=lambda item: item[1],
+                reverse=True
+            )
+            max_subject_confidence = max(
+                subject_items,
+                key=lambda item: item[1],
+                default=[None, 0.0]
+            )[1]
+            
+            # Get all subjects with max confidence (could be more than one)
+            most_confident_subjects_items = [
+                item
+                for item in subject_items
+                if item[1] == max_subject_confidence
+            ]
+
+            for winning_subject, s_confidence in most_confident_subjects_items:
+                if winning_subject is not None:
+                    winning_triples = []
+                    for col, linkage in enumerate(linkages):
+                        winning_links = []
+                            
+                        for link in linkage:
+                            if link.subject() == winning_subject:
+                                # Revise cea with cpa
+                                if len(self._cpa[col]) > 0:
+                                    if link.predicate() in self._cpa[col]:
+                                        winning_links.append((
+                                            link.get_triple(),  # Original triple
+                                            self._cpa[col][link.predicate()] * link.get_confidence()    # Predicate confidence AND link confidence
+                                        ))
+                        
+                        # Now take the most confident link from all candidates
+                        winning_triples.append(
+                            max (
+                                winning_links,
+                                key=lambda item: item[1],
+                                default=(None, 0)
+                            )
+                        )
+                            
+                    # subj_conf * (obj1_conf + obj2_conf + ... + objn_conf)
+                    row_confidence = s_confidence * sum([
+                        wt[1]
+                        for wt in winning_triples
+                    ])
+                    # Set winning row subject, linkages and confidence
+                    if row_confidence > winning_row_confidence:
+                        winning_row_subject = winning_subject
+                        winning_row_linkages = winning_triples
+                        winning_row_confidence = row_confidence
+
+            row_results.append((winning_row_subject, winning_row_linkages, winning_row_confidence))
+
+        return row_results
